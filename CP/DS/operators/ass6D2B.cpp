ffd3b1230d946f374224ca76a1f805401cf3431a
sonyD4d <sonyd4d@live.com>
2020-10-13 15:07:32 +0530

    sonyD4d

--- CP/DS/operators/ass6D2B.cpp
+++ CP/DS/operators/ass6D2B.cpp
@@ -1,1 +1,25 @@
-
+#include<iostream>
+#include<math.h>
+using namespace std;
+void d2b(long n){
+    if(n){
+        d2b(n/2);
+        cout<<n%2;
+    }
+}
+int main()
+{
+    long n;
+    cin>>n;
+    if(n<0||n>pow(10,5)){
+        cout<<"-1";
+        return 0;
+    }
+    if(n==0){
+        cout<<0;
+    }
+    else{
+        d2b(n);
+    }
+    return 0;
+}
